ea32470cc31acb2ff36ae0c2e83cc3ed3daa4326
hwanjari1 <harshal.wanjari@capgemini.com>
2017-06-26 13:46:39 +0530

    usage data

--- scripts/createNodeLib/ec2Method.py
+++ scripts/createNodeLib/ec2Method.py
@@ -31,7 +31,7 @@ class ec2Method:
 
     def getInstanceData(self):
         '''Get Data from running instance'''
-        while "%s"%ec2.get_only_instances(self.instanceid)[0].ip_address == 'None' || ec2.get_only_instances(self.instanceid)[0].state_code != 16:
+        while "%s"%ec2.get_only_instances(self.instanceid)[0].ip_address == 'None' or ec2.get_only_instances(self.instanceid)[0].state_code != 16:
             #wait for system come UP
             time.sleep(1)
         self.nodeData.setInstanceId(self.instanceid)
